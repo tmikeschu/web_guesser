5e915d918a70219d1af5c19e4a6362af226d48bd
Mike Schutte <tmikeschutte@gmail.com>
2016-11-16 10:31:32 +0800

    I4: guess count and reset

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,19 +1,39 @@
 require 'sinatra'
 require 'sinatra/reloader'
 
-set :secret_number, rand(101)
+@@guesses = 5
+@@message = ""
 
+set :secret_number, rand(101)
+disable :started
+
+  
 get '/' do
-  guess = params["guess"].to_i
-  message = check_guess(guess) 
-  erb :index, :locals => {:secret_number => settings.secret_number, :message => message}
+  check_status
+  @@guess = params["guess"].to_i
+  @@message = check_guess(@@guess) 
+  erb :index, :locals => {:secret_number => settings.secret_number, :message => @@message, :guesses => @@guesses}
 end
 
+def check_status
+  if @@guesses == 0 || @@message.include?("right")
+    settings.secret_number = rand(101)
+    @@guesses = 5
+    @@guess = 0
+  end
+end
+
+
 def check_guess(guess)
+  unless settings.started? 
+    settings.started = true
+    return "Let's play!"
+  end 
   message = "Too high!" if guess > settings.secret_number
   message = "Too low!" if guess < settings.secret_number
   message.insert(0, "Way ") if (settings.secret_number - guess).abs > 5
   message = "You got it right!" if guess == settings.secret_number
+  @@guesses -= 1
   message.capitalize
 end
 
