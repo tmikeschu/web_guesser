87669342bb4350c050163e2f9b6ec84f15e03032
Mike Schutte <tmikeschutte@gmail.com>
2016-11-16 09:02:51 +0800

    develops message maker

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,5 +4,18 @@ require 'sinatra/reloader'
 secret_number = rand(101)
 
 get '/' do
-  erb :index, :locals => {:secret_number => secret_number}
+  message = message_maker(params["guess"].to_i) 
+  erb :index, :locals => {:secret_number => secret_number, :message => message}
+end
+
+def message_maker(guess)
+  message = "Too high!" if guess > secret_number
+  message = "Too low!" if guess < secret_number
+  message.insert(0, "Way ")
+  message = "You got it right!" if guess == secret_number
+  message
+end
+
+get '/?' do
+  # "Too low!" if params["guess"].to_i > secret_number
 end
